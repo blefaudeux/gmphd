ee874dea5c98420993266b63b18f5b736c2711c5
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-12-12 20:51:10 +0100

    removing old crappy code

--- libGMPHD/headers/eigen_tools.h
+++ libGMPHD/headers/eigen_tools.h
@@ -13,16 +13,47 @@
 
 using namespace std;
 
-float pseudo_inv(const Eigen::MatrixXf *mat_in,
-                Eigen::MatrixXf *mat_out);
+float pseudo_inv(Eigen::MatrixXf const &mat_in,
+                 Eigen::MatrixXf & mat_out);
 
-float pseudo_inv_6(const Eigen::Matrix <float, 6,6> *mat_in,
-                Eigen::Matrix <float, 6,6> *mat_out);
+template <size_t size>
+float pseudo_inv(Eigen::Matrix <float, size,size> const & mat_in,
+                 Eigen::Matrix <float, size,size> & mat_out)
+{
+    Eigen::Matrix <float, size,size> U;
+    Eigen::Matrix <float, size,1> eig_val;
+    Eigen::Matrix <float, size,size> eig_val_inv;
+    Eigen::Matrix <float, size,size> V;
+    float det;
 
+    eig_val_inv = Eigen::MatrixXf::Identity(size,size);
 
-float pseudo_inv_12(const Eigen::Matrix <float, 12, 12> *mat_in,
-                Eigen::Matrix <float, 12, 12> *mat_out);
+    // Compute the SVD decomposition
+    Eigen::JacobiSVD<Eigen::MatrixXf> svd(*mat_in,
+                                          Eigen::ComputeThinU | Eigen::ComputeThinV);
 
+    eig_val = svd.singularValues();
+    U = svd.matrixU();
+    V = svd.matrixV();
 
+    // Compute pseudo-inverse
+    // - quick'n'dirty inversion of eigen matrix
+    for (int i = 0; i<size; ++i) {
+        if (eig_val(i,0) != 0.f)
+            eig_val_inv(i,i) = 1.f / eig_val(i,0);
+        else
+            eig_val_inv(i,i) = 0.f;
+    }
+
+    *mat_out = V.transpose() * eig_val_inv * U.transpose();
+
+    // Compute determinant from eigenvalues..
+    det = 1.f;
+    for (int i=0; i<size; ++i) {
+        det *= eig_val(i,0);
+    }
+
+    return det;
+}
 
 #endif // EIGEN_TOOLS_H
--- libGMPHD/src/eigen_tools.cpp
+++ libGMPHD/src/eigen_tools.cpp
@@ -1,18 +1,18 @@
 #include "eigen_tools.h"
 
 
-float pseudo_inv(const Eigen::MatrixXf *mat_in,
-                 Eigen::MatrixXf *mat_out) {
+float pseudo_inv(Eigen::MatrixXf const & mat_in,
+                 Eigen::MatrixXf &mat_out) {
   int dim = 0;
 
   // Get matrices dimension :
-  if (mat_in->cols () != mat_in->rows ()) {
+  if (mat_in.cols () != mat_in.rows ()) {
     THROW_ERR("Cannot compute matrix pseudo_inverse");
   } else {
-    dim = mat_in->cols ();
+    dim = mat_in.cols ();
   }
 
-  mat_out->resize (dim, dim);
+  mat_out.resize (dim, dim);
 
   Eigen::MatrixXf U (dim,dim);
   Eigen::MatrixXf eig_val (dim, 1);
@@ -24,7 +24,7 @@ float pseudo_inv(const Eigen::MatrixXf *mat_in,
   eig_val_inv = Eigen::MatrixXf::Identity(dim,dim);
 
   // Compute the SVD decomposition
-  Eigen::JacobiSVD<Eigen::MatrixXf> svd(*mat_in, Eigen::ComputeFullU | Eigen::ComputeFullV);
+  Eigen::JacobiSVD<Eigen::MatrixXf> svd(mat_in, Eigen::ComputeFullU | Eigen::ComputeFullV);
 
   eig_val = svd.singularValues();
   U = svd.matrixU();
@@ -39,7 +39,7 @@ float pseudo_inv(const Eigen::MatrixXf *mat_in,
       eig_val_inv(i,i) = 0.f;
   }
 
-  *mat_out = V.transpose() * eig_val_inv * U.transpose();
+  mat_out = V.transpose() * eig_val_inv * U.transpose();
 
   // Compute determinant from eigenvalues..
   det = 1.f;
@@ -50,82 +50,3 @@ float pseudo_inv(const Eigen::MatrixXf *mat_in,
   return det;
 }
 
-
-// Small hack to compute pseudo-inverse for a 6x6 matrix using SVD method
-float pseudo_inv_6(const Eigen::Matrix <float, 6,6> *mat_in,
-                   Eigen::Matrix <float, 6,6> *mat_out)
-{
-  Eigen::Matrix <float, 6,6> U;
-  Eigen::Matrix <float, 6,1> eig_val;
-  Eigen::Matrix <float, 6,6> eig_val_inv;
-  Eigen::Matrix <float, 6,6> V;
-  float det;
-
-  eig_val_inv = Eigen::MatrixXf::Identity(6,6);
-
-  // Compute the SVD decomposition
-  Eigen::JacobiSVD<Eigen::MatrixXf> svd(*mat_in,
-                                        Eigen::ComputeThinU | Eigen::ComputeThinV);
-
-  eig_val = svd.singularValues();
-  U = svd.matrixU();
-  V = svd.matrixV();
-
-  // Compute pseudo-inverse
-  // - quick'n'dirty inversion of eigen matrix
-  for (int i = 0; i<6; ++i) {
-    if (eig_val(i,0) != 0.f)
-      eig_val_inv(i,i) = 1.f / eig_val(i,0);
-    else
-      eig_val_inv(i,i) = 0.f;
-  }
-
-  *mat_out = V.transpose() * eig_val_inv * U.transpose();
-
-  // Compute determinant from eigenvalues..
-  det = 1.f;
-  for (int i=0; i<6; ++i) {
-    det *= eig_val(i,0);
-  }
-
-  return det;
-}
-
-float pseudo_inv_12(const Eigen::Matrix <float, 12, 12> *mat_in,
-                    Eigen::Matrix <float, 12, 12> *mat_out)
-{
-  Eigen::Matrix <float, 12, 12> U;
-  Eigen::Matrix <float, 12,  1> eig_val;
-  Eigen::Matrix <float, 12, 12> eig_val_inv;
-  Eigen::Matrix <float, 12, 12> V;
-  float det;
-
-  eig_val_inv = Eigen::MatrixXf::Identity(12,12);
-
-  // Compute the SVD decomposition
-  Eigen::JacobiSVD<Eigen::MatrixXf> svd(*mat_in,
-                                        Eigen::ComputeThinU | Eigen::ComputeThinV);
-
-  eig_val = svd.singularValues();
-  U = svd.matrixU();
-  V = svd.matrixV();
-
-  // Compute pseudo-inverse
-  // - quick'n'dirty inversion of eigen matrix
-  for (int i = 0; i<12; ++i) {
-    if (eig_val(i,0) != 0.f)
-      eig_val_inv(i,i) = 1.f / eig_val(i,0);
-    else
-      eig_val_inv(i,i) = 0.f;
-  }
-
-  *mat_out = V.transpose() * eig_val_inv * U.transpose();
-
-  // Compute determinant from eigenvalues..
-  det = 1.f;
-  for (int i=0; i<12; ++i) {
-    det *= eig_val(i,0);
-  }
-
-  return det;
-}
